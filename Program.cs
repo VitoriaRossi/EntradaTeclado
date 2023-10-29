6c6c17642201c8c44ce6078eced9c27e2906c74c
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-10-29 16:01:26 -0300

    Exibindo o conteúdo da variável.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome..:");
+string nomeUsuario = Console.ReadLine();
+Console.Write("Obrigado, ");
+Console.WriteLine(nomeUsuario);
+
